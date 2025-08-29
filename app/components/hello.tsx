9355e91b2c5883ac29390e098726552415db1df0
Ahmed Abbas <iahmedabbas7@gmail.com>
2025-08-30 00:01:50 +0500

    Edited some Files and Learned about Server & Client Components

---
+++ app/components/hello.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+const Hello = () => {
+  return (
+    <>
+      <h1 className="text-[2rem] text-sky-400 font-semibold">by Ahmed Abbas</h1>
+    </>
+  );
+};
+
+export default Hello;
