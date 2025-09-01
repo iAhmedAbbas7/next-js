bc7392ea2e3463768e63e9c5dde6c9061382d23d
Ahmed Abbas <iahmedabbas7@gmail.com>
2025-09-01 22:41:36 +0500

    Learned about the Loading UI's in Next JS

---
+++ app/loading.tsx
@@ -0,0 +1,9 @@
+const Loading = () => {
+  return (
+    <div className="flex items-center justify-center w-[100vw] h-[100vh] text-4xl text-red-500">
+      Loading ....
+    </div>
+  );
+};
+
+export default Loading;
